9e131bd38160ff6dd63557bdba5e4cbc895e66a6
GithubCoder2021 <91566024+GithubCoder2021@users.noreply.github.com>
2021-10-14 07:30:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ function add() {
   document.getElementById("score").innerHTML = "Score: " + score; 
   //updates the score every time the button is clicked
 }
+
+var cursors = 0;
+
+function buyCursor(){
+    var cursorCost = Math.floor(10 * Math.pow(1.1,cursors));     //works out the cost of this cursor
+    if(score >= cursorCost){                                   //checks that the player can afford the cursor
+        cursors = cursors + 1;                                   //increases number of cursors
+    	score = score - cursorCost;                          //removes the cookies spent
+        document.getElementById('cursors').innerHTML = cursors;  //updates the number of cursors for the user
+        document.getElementById('cookies').innerHTML = score;  //updates the number of cookies for the user
+    };
+    var nextCost = Math.floor(10 * Math.pow(1.1,cursors));       //works out the cost of the next cursor
+    document.getElementById('cursorCost').innerHTML = nextCost;  //updates the cursor cost for the user
+};
+
+window.setInterval(function(){
+	
+	cookieClick(cursors);
+	
+}, 1000);
