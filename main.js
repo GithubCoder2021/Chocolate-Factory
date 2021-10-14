26f354903212d3294b3017f0427bebb952345499
GithubCoder2021 <91566024+GithubCoder2021@users.noreply.github.com>
2021-10-14 07:49:29 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 var cookies = 0;
 
-function cookieClick(number){
-    cookies = cookies + number;
-    document.getElementById("cookies").innerHTML = cookies;
+function clicksClick(number){
+    clicks = clicks + number;
+    document.getElementById("clicks").innerHTML = clicks;
 };
 
 var cursors = 0;
 
 function buyCursor(){
     var cursorCost = Math.floor(10 * Math.pow(1.1,cursors));     //works out the cost of this cursor
-    if(cookies >= cursorCost){                                   //checks that the player can afford the cursor
+    if(clicks >= cursorCost){                                   //checks that the player can afford the cursor
         cursors = cursors + 1;                                   //increases number of cursors
-    	cookies = cookies - cursorCost;                          //removes the cookies spent
+    	clicks = clicks - cursorCost;                          //removes the cookies spent
         document.getElementById('cursors').innerHTML = cursors;  //updates the number of cursors for the user
-        document.getElementById('cookies').innerHTML = cookies;  //updates the number of cookies for the user
+        document.getElementById('cookies').innerHTML = cl;  //updates the number of cookies for the user
     };
     var nextCost = Math.floor(10 * Math.pow(1.1,cursors));       //works out the cost of the next cursor
     document.getElementById('cursorCost').innerHTML = nextCost;  //updates the cursor cost for the user
@@ -21,6 +21,6 @@ function buyCursor(){
 
 window.setInterval(function(){
 	
-	cookieClick(cursors);
+	clicksClick(cursors);
 	
 }, 1000);
