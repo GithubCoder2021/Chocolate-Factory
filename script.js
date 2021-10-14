984df041328a797c5bcaa255f093246cca13f394
GithubCoder2021 <91566024+GithubCoder2021@users.noreply.github.com>
2021-10-14 07:41:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-var clicks = 0;
-function add() {
-  score++
-  //We are using the document.getElementById('').innerHTML method
-  document.getElementById("clicks").innerHTML = "Score: " + clicks; 
-  //updates the score every time the button is clicked
-}
+var cookies = 0;
+
+function cookieClick(number){
+    cookies = cookies + number;
+    document.getElementById("cookies").innerHTML = cookies;
+};
 
 var cursors = 0;
 
 function buyCursor(){
     var cursorCost = Math.floor(10 * Math.pow(1.1,cursors));     //works out the cost of this cursor
-    if(score >= cursorCost){                                   //checks that the player can afford the cursor
+    if(cookies >= cursorCost){                                   //checks that the player can afford the cursor
         cursors = cursors + 1;                                   //increases number of cursors
-    	clicks = clicks - cursorCost;                          //removes the clicks spent
+    	cookies = cookies - cursorCost;                          //removes the cookies spent
         document.getElementById('cursors').innerHTML = cursors;  //updates the number of cursors for the user
-        document.getElementById('clicks').innerHTML = clicks;  //updates the number of cookies for the user
+        document.getElementById('cookies').innerHTML = cookies;  //updates the number of cookies for the user
     };
     var nextCost = Math.floor(10 * Math.pow(1.1,cursors));       //works out the cost of the next cursor
     document.getElementById('cursorCost').innerHTML = nextCost;  //updates the cursor cost for the user
@@ -22,6 +21,6 @@ function buyCursor(){
 
 window.setInterval(function(){
 	
-	click(cursors);
+	cookieClick(cursors);
 	
 }, 1000);
