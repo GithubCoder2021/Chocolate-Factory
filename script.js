afc19ddcaaeaa0ebbe1dd0103dad4db63fcb080b
GithubCoder2021 <91566024+GithubCoder2021@users.noreply.github.com>
2022-07-15 11:53:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ function increment() {
 }
 var workers = 0;
 var workersCost = 25;
+var cursors = 0;
+var cursorsCost = 0;
 
 setInterval(function() {
   chocolates += chocoPerSecond;
@@ -34,6 +36,31 @@ function buyWorker() {
 	        chocoPerSecond += 1; // increases number of apples per second
 
 	        document.getElementById("showChocolatesPerSecond").innerText = chocoPerSecond;	
+
+	}	else { 
+		alert("Get more Chocolates!") 
+	}
+}
+
+function buyCursor() {
+   
+ if (chocolates >= cursorsCost) { 
+		
+		chocolates -= cursorsCost; 
+
+		document.getElementById("showChocolates").innerText = chocolates; 
+
+		cursors++;
+
+		document.getElementById("cursorsNum").innerText = cursors;
+	 
+	        cursorsCost *=3; 
+
+	        document.getElementById("cursorsCost").innerText = cursorsCost;
+
+	        chocoPerClick *= 2; 
+
+	        document.getElementById("showChocolatesPerClick").innerText = chocoPerClick;	
 
 	}	else { 
 		alert("Get more Chocolates!") 
